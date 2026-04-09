16f4e06209ac86ce6dfbafa4777f56d9704f6b3f
LorenzoRottigni <lorenzo@rottigni.net>
2026-04-09 21:51:42 +0200

    feat: :sparkles: mixing up nodes and graphs

--- crates/examples/src/graph.rs
+++ crates/examples/src/graph.rs
@@ -2,7 +2,13 @@ use graphio_macro::graph;
 use crate::node::Context;
 
 graph! {
-    name: DataGraph,
+    name: DataGraph1,
     context: Context,
     nodes: [crate::node::get_data >> crate::node::validate_data >> crate::node::normalize_data >> crate::node::print_data & crate::node::send_email & crate::node::publish_event >> crate::node::disconnect_from_db]
+}
+
+graph! {
+    name: DataGraph2,
+    context: Context,
+    nodes: [crate::node::get_data >> crate::node::validate_data >> crate::node::normalize_data >> crate::node::print_data & crate::node::send_email & crate::node::publish_event >> crate::node::disconnect_from_db >> DataGraph1::run]
 }
