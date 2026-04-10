c0ff98dd7f0fe4e2e41c669bcda500e6f3c8c6e2
LorenzoRottigni <lorenzo@rottigni.net>
2026-04-10 15:42:07 +0200

    feat: :sparkles: TODO

--- crates/examples/src/graph.rs
+++ crates/examples/src/graph.rs
@@ -38,3 +38,29 @@ graph! {
         crate::node::disconnect_from_db
     ]
 }
+
+/*
+graph! {
+    name: DataGraph,
+    context: Context,
+    nodes: [
+        crate::node::get_data >>
+        crate::node::validate_data >>
+        @route {
+            on: |ctx: &mut Context| Status::Invalid,
+            routes: {
+                Status::Valid => crate::node::print_data & crate::node::send_email,
+                Status::Invalid => crate::node::print_error,
+                Status::NeedsReview => crate::node::send_review,
+            }
+        }
+        >>
+        @loop {
+            condition: |ctx: &mut Context, i: usize| i < 3,
+            body: crate::node::print_data
+        }
+        >>
+        crate::node::disconnect_from_db
+    ]
+}
+*/
